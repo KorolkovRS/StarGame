2796c5c4549cc330fe9ef006784a5c440cd3b175
Roman Korolkov <korolkovrs@yandex.ru>
2020-10-13 23:33:22 +0300

    1) Добавлена отрисовка фона (земля и background). 2) Лого перемещается по экрану.

--- core/src/ru/korolkovrs/SpaceGame.java
+++ core/src/ru/korolkovrs/SpaceGame.java
@@ -9,19 +9,27 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class SpaceGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture background;
+	Texture ground;
+
+	int counter = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		background = new Texture("background.png");
+		ground = new Texture("groundDirt.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 100, 100);
+		drawBackground();
+		drawGround();
+		drawMobilObject();
 		batch.end();
 	}
 	
@@ -29,5 +37,20 @@ public class SpaceGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+
+	private void drawBackground() {
+		batch.draw(background, 0,0);
+	}
+
+	private void drawGround() {
+		batch.draw(ground, 0,0);
+	}
+
+	private void drawMobilObject() {
+		if (counter >= Gdx.graphics.getWidth()) {
+			counter = 0;
+		}
+		batch.draw(img, ++counter, 100);
 	}
 }
